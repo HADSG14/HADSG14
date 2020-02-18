f1d26130efb39a27e11f0d7fdfddf37a9d0a7d07
hashi <indigarci98@gmail.com>
2020-02-18 13:33:12 +0100

    email correcto y acceso a datos

---
+++ AccesoDatos/AccesoDatos/Class1.vb
@@ -0,0 +1,49 @@
+﻿Imports System.Data.SqlClient
+
+Public Class Class1
+    Private Shared conexion As New SqlConnection
+    Private Shared comando As New SqlCommand
+
+    Public Shared Function conectar() As String
+        Try
+            conexion.ConnectionString = “Server=tcp:hads-g14a.database.windows.net,1433;Initial Catalog=HADS-G14A;Persist Security Info=False;User ID=gontxi@live.com@hads-g14a;Password=Gonzalo1998;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+            conexion.Open()
+        Catch ex As Exception
+            Return False
+
+        End Try
+        Return True
+
+    End Function
+
+    Public Shared Function insertar(ByVal nombre As String) As String
+        conectar()
+
+        Dim st = "insert into Usuarios values ('" & nombre & " ','A',' a'," & 0 & "," & 0 & ",' n ',' n  '," & 0 & ")"
+        Dim numregs As Integer
+        comando = New SqlCommand(st, conexion)
+        Try
+            numregs = comando.ExecuteNonQuery()
+        Catch ex As Exception
+            cerrarconexion()
+
+            Return ex.Message
+        End Try
+        cerrarconexion()
+
+
+        Return (numregs & " registro(s) insertado(s) en la BD ")
+    End Function
+
+
+    Public Shared Function obtenerdatos() As SqlDataReader
+        Dim st = "select * from tabla"
+        comando = New SqlCommand(st, conexion)
+        Return (comando.ExecuteReader())
+    End Function
+
+    Public Shared Sub cerrarconexion()
+        conexion.Close()
+    End Sub
+
+End Class
--- EmailSender/EmailSender/Class1.vb
+++ EmailSender/EmailSender/Class1.vb
@@ -2,36 +2,22 @@
 Imports System.Net.NetworkAccess
 Public Class Class1
 
-    Public Function enviarEmail(ByVal email As String, ByRef msg As String) As Boolean
+    Public Function enviarEmail(ByVal email As String, ByRef msge As String) As Boolean
         Try
-            'Direccion de origen
-            Dim from_address As New MailAddress("hadsg14@gmail.com", "HadsG14")
-            'Direccion de destino
-            Dim to_address As New MailAddress(email)
-            'Password de la cuenta
-            Dim from_pass As String = "asiergonzalo14"
-            'Objeto para el cliente smtp
-            Dim smtp As New SmtpClient
-            'Host en este caso el servidor de gmail
-            smtp.Host = "smtp.gmail.com"
-            'Puerto
-            smtp.Port = 587
-            'SSL activado para que se manden correos de manera segura
-            smtp.EnableSsl = True
-            'No usar los credenciales por defecto ya que si no no funciona
-            smtp.UseDefaultCredentials = False
-            'Credenciales
-            smtp.Credentials = New System.Net.NetworkCredential(from_address.Address, from_pass)
-            'Creamos el mensaje con los parametros de origen y destino
-            Dim message As New MailMessage(from_address, to_address)
-            'Añadimos el asunto
-            message.Subject = "G14"
-            'Concatenamos el cuerpo del mensaje a la plantilla
-            message.Body = "hola"
-            'Definimos el cuerpo como html para poder escojer mejor como lo mandamos
-            message.IsBodyHtml = True
-            'Se envia el correo
-            smtp.Send(message)
+            Dim msg = New MailMessage()
+            msg.To.Add(New MailAddress(email, "HADS"))
+            msg.From = New MailAddress("ggaldos002@ikasle.ehu.eus", "HADS")
+            msg.Subject = "G14"
+            msg.Body = msge
+            msg.IsBodyHtml = True
+            Dim client = New SmtpClient()
+            client.UseDefaultCredentials = False
+            client.Credentials = New System.Net.NetworkCredential("ggaldos002@ikasle.ehu.eus", "Gonzalito1998")
+            client.Port = 587
+            client.Host = "smtp.ehu.eus"
+            client.DeliveryMethod = SmtpDeliveryMethod.Network
+            client.EnableSsl = True
+            client.Send(msg)
         Catch e As Exception
             Return False
         End Try
